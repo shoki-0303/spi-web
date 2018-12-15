807e292153a418415b96049aad710f3448e073ad
shoki-0303 <shoki522.33333@gmail.com>
2018-12-15 14:41:37 +0900

    set Group admin

---
+++ app/controllers/admin.go
@@ -0,0 +1,11 @@
+package controllers
+
+import (
+	"github.com/labstack/echo"
+)
+
+// AdminMiddleWare : done before processeing related to admin
+func AdminMiddleWare(next echo.HandlerFunc) echo.HandlerFunc {
+	//"ここにadminUserを確かめる処理を書く"
+	return next
+}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"spi-web/app/controllers"
 	"spi-web/app/models"
 	"spi-web/config"
 	"spi-web/utils"
@@ -14,6 +15,9 @@ func main() {
 	utils.LoggingSetting(config.Config.LogFile)
 	defer models.Db.Close()
 	e := echo.New()
+
+	adminGroup := e.Group("/admin")
+	adminGroup.Use(controllers.AdminMiddleWare)
 
 	if err := e.Start(fmt.Sprintf(":%d", config.Config.Port)); err != nil {
 		log.Fatalf("ListenAndServe err=%s", err)
