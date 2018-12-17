75d3b3f5a30d47a133cbd313cf056c021d5c3e58
shoki-0303 <shoki522.33333@gmail.com>
2018-12-17 13:00:23 +0900

    create html renderer

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"io"
 	"log"
 	"spi-web/app/controllers"
 	"spi-web/app/models"
@@ -12,14 +14,29 @@ import (
 	"github.com/labstack/echo/middleware"
 )
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+var t = &Template{
+	templates: template.Must(template.ParseGlob("app/views/*.html")),
+}
+
 func main() {
 	utils.LoggingSetting(config.Config.LogFile)
 	defer models.Db.Close()
+
 	e := echo.New()
+	e.Renderer = t
 	e.Use(middleware.Logger())
 
 	adminGroup := e.Group("/admin")
 	adminGroup.Use(controllers.AdminMiddleWare)
+	adminGroup.GET("/register", controllers.AdminRegister)
 	adminGroup.POST("/user", controllers.AdminCreateUser)
 
 	if err := e.Start(fmt.Sprintf(":%d", config.Config.Port)); err != nil {
