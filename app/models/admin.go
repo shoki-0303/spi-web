1127d92c1fafd5c8cfac1ca473dafca7bf38be25
shoki-0303 <shoki522.33333@gmail.com>
2018-12-18 16:40:00 +0900

    fix hardcode

--- app/models/admin.go
+++ app/models/admin.go
@@ -7,17 +7,22 @@ import (
 	"spi-web/app/models/helpers"
 )
 
+type AdminUser struct {
+	Name           string
+	Email          string
+	HashedPassword string
+}
+
 var tablename = "admin_users"
 
-func CreateAdminUser() error {
+func CreateAdminUser(adminUser *AdminUser) error {
 	err := helpers.WithTransaction(Db, func(tx *sql.Tx) error {
 		cmd := fmt.Sprintf(`INSERT INTO %s (name, email, password) VALUES (?, ?, ?)`, tablename)
-		_, err := tx.Exec(cmd, "user001", "user001@gmail.com", "0987654321")
+		_, err := tx.Exec(cmd, adminUser.Name, adminUser.Email, adminUser.HashedPassword)
 		if err != nil {
 			log.Printf("action=CreateAdminUser err=%s", err)
 		}
 		return err
 	})
-
 	return err
 }
