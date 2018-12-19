0dc8fd3eaf21fd81a7be311f2cd19524a1fca0da
shoki-0303 <shoki522.33333@gmail.com>
2018-12-19 20:49:17 +0900

    create getAdminUser

--- app/models/admin.go
+++ app/models/admin.go
@@ -26,3 +26,18 @@ func CreateAdminUser(adminUser *AdminUser) error {
 	})
 	return err
 }
+
+func GetAdminUser(name string) (*AdminUser, error) {
+	var adminUser AdminUser
+	err := helpers.WithTransaction(Db, func(tx *sql.Tx) error {
+		cmd := fmt.Sprintf(`SELECT name FROM %s WHERE name = ?`, tablename)
+		row := tx.QueryRow(cmd, name)
+		err := row.Scan(&adminUser.Name)
+		if err != nil {
+			log.Printf("action=GetAdminUser err=%s", err)
+		}
+		return err
+	})
+	fmt.Println(adminUser)
+	return &adminUser, err
+}
